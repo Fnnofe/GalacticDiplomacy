b272c4e653b189dc5e60eedd9fe3c6f4f3be98f6
BloodSample <>
2025-03-11 20:28:31 +0400

    Made first puzzle logic for the inspection

--- GalDip/Assets/Ahmed/Scripts/InteractWithObject.cs
+++ GalDip/Assets/Ahmed/Scripts/InteractWithObject.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,10 @@ public class InteractWithObject : MonoBehaviour
 {
     private RaycastHit ray;
     [HideInInspector] public GameObject go;
- 
+    
+
+   
+
     void Update()
     {
         var inspectedobject = GetFacingObject();
